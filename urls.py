3c76a3154d09af0766df324edf35708ac9b272f0
shenihachris <139878409+shenihachris@users.noreply.github.com>
2024-04-11 22:07:06 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('cars/', include('cars.urls')),
-    path('accounts/', include('accounts.urls')),
-    # path('socialaccounts/', include('allauth.urls')),
-    path('contacts/', include('contacts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
